a279e224e1a2b72dbd095bb6b24788e3f5076c13
Obake18 <coimbrajuniorm@gmail.com>
2024-04-14 19:55:35 -0300

    Atualizações no entity

--- src/jogadores/entities/jogadore.entity.ts
+++ src/jogadores/entities/jogadore.entity.ts
@@ -1,1 +1,19 @@
-export class Jogadore {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Jogador {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nickName: string;
+
+  @Column()
+  fotoDePerfil: string; // Armazena o caminho para a imagem
+
+  @Column()
+  senha: string;
+
+  @Column()
+  recorde: number;
+}
