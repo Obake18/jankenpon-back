006731f2ae04a28a7a5d600118b572479b9ed347
Obake18 <coimbrajuniorm@gmail.com>
2024-04-14 19:09:01 -0300

    banco de dados supostamente funcionando.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dpg-co1ggmed3nmc73a0bbqg-a',
+      port: 5432,
+      username: 'aula_milton_user',
+      password: 'jJ8mAjGLazggbReYFgc63fQ0Rr3MVtjG',
+      database: 'aula_milton',
+      entities: [/* Adicione suas entidades aqui */],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
